ae1f9df6192304224c4bde961da550874e22fa74
vaibhav gupta <vaibhav.gupta3659@gmail.com>
2022-10-07 10:38:10 +0530

    Updated

---
+++ class_decrypt.cpp
@@ -0,0 +1,14 @@
+#include <cstring>
+#include <fstream>
+#include <iostream>
+#include <sstream>
+
+#include "structures.h"
+
+using namespace std;
+
+class Decrypt {
+ private:
+  string msgstr;
+  string keystr;
+};
